8550296e8765a610f30337a99ffa0d33c5108db6
hejiawang <952327407@qq.com>
2017-08-11 22:30:07 +0800

    test

---
+++ src/com/wang/java8/test/Test2.java
@@ -0,0 +1,16 @@
+package com.wang.java8.test;
+
+import java.util.function.Consumer;
+
+/**
+ * 消费型接口示例
+ */
+public class Test2 {
+
+    public static void donation(Integer money, Consumer<Integer> consumer){
+        consumer.accept(money);
+    }
+    public static void main(String[] args) {
+        donation(1000, money -> System.out.println("好心的麦乐迪为Blade捐赠了"+money+"元")) ;
+    }
+}
